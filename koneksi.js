0245e59bae84de90aeed7644c52f74cb7dddb643
ranggabn12 <rbaghas32@gmail.com>
2023-10-04 17:08:44 +0700

    update config

--- koneksi.js
+++ koneksi.js
@@ -1,12 +1,5 @@
 var mysql = require("mysql");
 
-// const pool = mysql.createPool({
-//   host: "remotemysql.com",
-//   port: "3306",
-//   user: "n8iU6eOeCM",
-//   password: "Yke6P5cMSE",
-//   database: "n8iU6eOeCM",
-// });
 const pool = mysql.createPool({
   host: "localhost",
   port: "3306",
